701aad97a618b009d4c312219b3328144b2f057e
NancyShiwal <nancy.shiwal08@gmail.com>
2026-06-13 17:38:02 +0530

    Add files via upload

---
+++ day2_Variables_datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 123;
+    cout << "The value of a is: " << a << endl;
+
+    char b = 'V';
+    cout << "The value of b is: " << b << endl;
+
+    bool bl = true;
+    cout << "The value of bl is: " << bl << endl;
+
+    float f = 1.2;
+    cout << "The value of f is: " << f << endl;
+
+    double d = 1.23;
+    cout << "The value of d is: " << d << endl;
+    
+}
