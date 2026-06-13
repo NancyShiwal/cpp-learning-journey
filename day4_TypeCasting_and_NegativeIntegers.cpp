139a12a11b4d982491ad4f0ba37dfd350f168ad9
NancyShiwal <nancy.shiwal08@gmail.com>
2026-06-13 18:10:02 +0530

    Add files via upload

---
+++ day4_TypeCasting_and_NegativeIntegers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 'a';
+    cout << a << endl; // This will print the ASCII value of 'a', which is 97
+
+    char b = 106;
+    cout << b << endl; // This will print the character corresponding to ASCII value 106, which is 'j'  
+
+    int c = -10;
+    cout << c << endl; // This will print -10
+
+    unsigned int d = -32;
+    cout << d << endl; // This will print a large positive number due to unsigned representation
+    
+}
