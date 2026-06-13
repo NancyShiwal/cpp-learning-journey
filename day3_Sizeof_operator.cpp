d54e4aa93e633b446d7066936281039eb7da42e6
NancyShiwal <nancy.shiwal08@gmail.com>
2026-06-13 17:47:52 +0530

    Add files via upload

---
+++ day3_Sizeof_operator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 123;
+    cout << "The value of a is: " << a << endl;
+
+    char b = 'V';
+    cout << "The value of b is: " << b << endl;
+
+    bool bl = true;
+    cout << "The value of bl is: " << bl << endl;
+
+    float f = 1.2;
+    cout << "The value of f is: " << f << endl;
+
+    double d = 1.23;
+    cout << "The value of d is: " << d << endl;
+
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+
+}
+    
