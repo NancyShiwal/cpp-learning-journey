78a682b1386c6de4addc7b1b52a40cfe37c85697
NancyShiwal <nancy.shiwal08@gmail.com>
2026-06-19 17:41:08 +0530

    Add files via upload

---
+++ day5_ArithematicOperators_and_RelationalOperators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "\nArithmetic Operators\n";
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+
+    cout << "\nRelational Operators\n";
+    cout << "a == b : " << (a == b) << endl;
+    cout << "a != b : " << (a != b) << endl;
+    cout << "a > b : " << (a > b) << endl;
+    cout << "a < b : " << (a < b) << endl;
+    cout << "a >= b : " << (a >= b) << endl;
+    cout << "a <= b : " << (a <= b) << endl;
+
+}
